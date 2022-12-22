56c928e2fedbd4e81d0997ee803eb65459563e8e
Justus B <justus.c.brown@gmail.com>
2022-12-22 12:41:52 -0800

    entity models

---
+++ src/main/java/com/technews/VariableTypes.java
@@ -0,0 +1,10 @@
+package com.technews;
+
+public class VariableTypes {
+    public static void main(String[] args){
+        String joe =  "bonby";
+        double price = 1.3434;
+        boolean yes = true;
+        System.out.println(joe);
+    }
+}
---
+++ src/main/java/com/technews/VariablesPractice.java
@@ -0,0 +1,12 @@
+package com.technews;
+
+public class VariablesPractice {
+    public static void main(String[] args){
+        int numOne = 1;
+        double numTwo = 7.63;
+        String test = "testy";
+        System.out.println(numOne +numTwo);
+        System.out.println(numOne + test);
+
+    }
+}
